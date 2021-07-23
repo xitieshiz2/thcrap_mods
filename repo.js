13d9cacb3eacb500d17331834e4014015b6d30d5
XiTieShiZ2 <xitieshi@outlook.com>
2021-07-23 15:44:31 +0800

    fix

--- repo.js
+++ repo.js
@@ -2,10 +2,10 @@
 	"contact": "xitieshi@outlook.com",
 	"id": "xitieshiz",
 	"patches": {
-		"alphes-cn": "Add Chinese support for DTM/alphes",
+		"alphes-cn": "Add Chinese support for DTM/alphes"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/xitieshiz2/thcrap_mods/master/"
 	],
-	"title": "XiTieShiZ's thcrap mods"
+	"title": "XiTieShiZ's thprac mods"
 }
